fedd3d3f3a105b250680ea1386e803a3a28f2385
TSechrist <thomas.sechrist@gmail.com>
2022-08-07 11:53:36 -0700

    Inital commit to build window

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # toste_game
+
+# This is a game engine using pygame library
+# Tom Sechrist Stephanie Sechrist
+# 08.07.22
---
+++ src/main.py
@@ -0,0 +1,36 @@
+import pygame
+import os
+
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Toste_engine")
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+PLAYER_1_IMAGE
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+
+    
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()

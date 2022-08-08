f77fe6096fcb7e5ac1e3b9e76ee8d215df227af0
TSechrist <thomas.sechrist@gmail.com>
2022-08-07 18:22:55 -0700

    Moving 4 directions and border

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import os
 
 WIDTH, HEIGHT = 800, 600
 ICON_WIDTH, ICON_HEIGHT = 50, 50
+VEL = 5
+
+border = pygame.Rect(0, 0, WIDTH, HEIGHT)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Toste_engine")
 
@@ -15,6 +18,16 @@ PLAYER_1_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('../res',
 CHEST_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join('../res', 'chest.png')), (ICON_WIDTH, ICON_HEIGHT))
 
 
+
+def player_handle_movement(keys_pressed, player):
+    if(keys_pressed[pygame.K_w] and player.y > 0): #Up
+        player.y -= VEL
+    if(keys_pressed[pygame.K_s] and player.y + player.height < border.height): #Down
+        player.y += VEL
+    if(keys_pressed[pygame.K_a] and player.x > 0): #Left
+        player.x -= VEL
+    if(keys_pressed[pygame.K_d] and player.x + player.width < border.width): #Right
+        player.x += VEL
 
 def draw_window(player, chest):
     WIN.fill(GREY)
@@ -29,7 +42,6 @@ def main():
     player = pygame.Rect(300, 100, ICON_WIDTH, ICON_HEIGHT)
     chest = pygame.Rect(600, 400, ICON_WIDTH, ICON_HEIGHT)
 
-
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -37,11 +49,8 @@ def main():
                 run = False
 
         keys_pressed = pygame.key.get_pressed()
-        if(keys_pressed[pygame.K_d]):
-            player.x += 1
-
+        player_handle_movement(keys_pressed, player)
         draw_window(player, chest)
-
     
     pygame.quit()
 

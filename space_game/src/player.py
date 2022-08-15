29164040797650a7546b0c96363d28aa856e208b
Tom Laptop <thomas.sechrist@gmail.com>
2022-08-14 21:41:36 -0700

    Added more controls for spacegame

--- space_game/src/player.py
+++ space_game/src/player.py
@@ -4,5 +4,36 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../res/test/player.png').convert_alpha()
+        self.image = pygame.transform.scale(pygame.image.load('../res/test/player.png').convert_alpha(), (64, 64))
         self.rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w] and keys[pygame.K_s]:
+            self.direction.y = 0
+        elif keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_a] and keys[pygame.K_d]:
+            self.direction.x = 0
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed):
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
--- space_game/src/tile.py
+++ space_game/src/tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../res/test/rock.png').convert_alpha()
+        self.image = pygame.transform.scale(pygame.image.load('../res/test/rock_1.png').convert_alpha(), (64, 64))
         self.rect = self.image.get_rect(topleft = pos)

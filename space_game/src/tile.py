577b7db832a472fa9d8f60c88b273c3a3865ba2a
Tom Laptop <thomas.sechrist@gmail.com>
2022-08-19 16:59:10 -0700

    Camera and overlap working

--- space_game/src/tile.py
+++ space_game/src/tile.py
@@ -6,3 +6,4 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.transform.scale(pygame.image.load('../res/test/rock_1.png').convert_alpha(), (64, 64))
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -18)
